0545ae2b6d070af3f9235a02896b4334f58ca884
Сол Коган <45554270+solkogan@users.noreply.github.com>
2020-05-22 13:05:03 +0600

    Add files via upload

---
+++ translate_example.py
@@ -0,0 +1,10 @@
+from googletrans import Translator
+translator = Translator()
+
+# Переводим с русского на английский
+w=translator.translate('Привет. Как дела?', dest='en')
+print(w.text)
+
+# Переводим с английского на русский
+w=translator.translate('How much is the fish in ocean?', dest='ru')
+print(w.text)
